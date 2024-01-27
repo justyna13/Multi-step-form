7134042bb99a498dd27b5a89dbaf3512a7f9de86
justynalodzinska <justyna.lodzinska@smartive.app>
2024-01-27 20:23:05 +0100

    cypress first step errors added

--- cypress/e2e/form.cy.ts
+++ cypress/e2e/form.cy.ts
@@ -1,4 +1,5 @@
-import { beforeEach } from "mocha"
+import { beforeEach } from "mocha";
+const formPage = require('../pages/FormPage');
 
 describe('home page', () => {
   beforeEach(() => {
@@ -6,9 +7,36 @@ describe('home page', () => {
   });
 
   context('First step', () => {
-    it('passes', () => {
-      console.log('first test');
-    })
+    it('should go to next step when fields filled', () => {
+      formPage.elements.getFullNameInput().type('First name');
+      formPage.elements.getEmailInput().type('test@exampl.com');
+      formPage.elements.getPhoneInput().type('123123123');
+      formPage.elements.getNextStepBtn().should('exist');
+
+      formPage.clickNextStepBtn();
+      formPage.elements.getActiveStepTitle().should('contain', 'Select your plan');
+    });
+
+    it('should show error when clicked next step and fields are empty', () => {
+      formPage.clickNextStepBtn();
+
+      cy.get('[data-testid="form-input-name"] > .label-wr > .error-msg').should('contain', 'This field is required');
+      cy.get('[data-testid="form-input-phone"] > .label-wr > .error-msg').should('contain', 'This field is required');
+      cy.get('[data-testid="form-input-email"] > .label-wr > .error-msg').should('contain', 'This field is required');
+      formPage.elements.getActiveStepTitle().should('not.contain', 'Select your plan');
+    });
+
+    it('should show email error when submit on invalid field value', () => {
+      formPage.elements.getEmailInput().type('testxampl.com');
+      formPage.clickNextStepBtn();
+      cy.get('[data-testid="form-input-email"] > .label-wr > .error-msg').should('contain', 'Invalid email address');
+    });
+
+    it('should show phone error when submit on invalid field value', () => {
+      formPage.elements.getPhoneInput().type('123');
+      formPage.clickNextStepBtn();
+      cy.get('[data-testid="form-input-phone"] > .label-wr > .error-msg').should('contain', 'Invalid phone number');
+    });
   });
 
   context('Second step', () => {
